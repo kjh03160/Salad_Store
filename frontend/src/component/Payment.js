96a8d7c22012e107f5c00faccb8facb173104103
rkdrud34 <rkdrud34@nate.com>
2020-12-01 19:27:28 +0900

    test

--- frontend/src/component/Payment.js
+++ frontend/src/component/Payment.js
@@ -5,43 +5,6 @@ import {Redirect} from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
-
-
-const StyledButton = styled.button`
-  /* 공통 스타일 */
-  display: inline-flex;
-  outline: none;
-  border: none;
-  border-radius: 4px;
-  color: white;
-  font-weight: bold;
-  cursor: pointer;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  justify-content:center;
-  align-items:center;
-  /* 크기 */
-  height: 2.25rem;
-  font-size: 1rem;
-  
-
-  /* 색상 */
-  background: skyblue;
-  &:hover {
-    background: ${lighten(0.01), "228be6"};
-  }
-  &:active {
-    background: ${darken(0.5), "228be6"};
-  }
-
-  /* 기타 */
-  & + & {
-    margin-left: 1rem;
-  }
-`;
-
-
-
 const DarkBackground = styled.div`
   position: fixed;
   left: 0;
