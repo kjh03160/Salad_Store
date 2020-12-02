21af8396e4653ff69c349a0b17b58af2cd851c18
kjh03160 <kis03160@likelion.org>
2020-12-02 11:46:55 +0900

    edit method.py

--- backend/API/method.py
+++ backend/API/method.py
@@ -1,14 +1,16 @@
-
 import pandas as pd
 import re
 
 def snake_to_camel(query_result):
-    data = query_result[0]
-    keys = data.keys()
-    for snake_style in keys:
-        camel_style = re.sub('_[a-z]', lambda m : m.group().upper().replace('_', ""), snake_style)
-        data[camel_style] = data.pop(snake_style)    
-    return [data]
+    result = []
+    for i in range(len(query_result)):
+        data = query_result[i]
+        keys = list(data.keys())
+        for snake_style in keys:
+            camel_style = re.sub('_[a-zA-Z]', lambda m : m.group().upper().replace('_', ""), snake_style)
+            data[camel_style] = data.pop(snake_style)    
+        result.append(data)
+    return result
 
 # SQLAlchemy query proxy to dictionary
 def query_to_dict(ret):
@@ -23,16 +25,16 @@ def query_to_dict(ret):
 # N * M * K rows SQL query to N rows list 
 def many_to_one(data):
     df = pd.DataFrame(data)
-    orders = map(int, df['order_pk'].unique())
+    orders = map(int, df['orderPk'].unique())
     order_list = []
     for order in orders:
-        order_df = df[df['order_pk'] == order]
-        order_dict = {'order_pk' : order, 'order_time' : order_df.iloc[0]['order_time'], 'menus' : []}
-        products = map(int, order_df['product_pk'].unique())
+        order_df = df[df['orderPk'] == order]
+        order_dict = {'orderPk' : order, 'orderTime' : order_df.iloc[0]['orderTime'], 'menus' : [], 'completed' : bool(order_df.iloc[0]['completed'])}
+        products = map(int, order_df['productPk'].unique())
         for product in products:
-            product_df = order_df[order_df['product_pk'] == product]
-            product_dict = {'product_pk' : product, 'menu_name' : product_df.iloc[0]['menu_name'], 'quantity' : int(product_df.iloc[0]['quantity']),
-                            'options' : product_df['option_name'].values.tolist()}
+            product_df = order_df[order_df['productPk'] == product]
+            product_dict = {'productPk' : product, 'menuName' : product_df.iloc[0]['menuName'], 'quantity' : int(product_df.iloc[0]['quantity']),
+                            'options' : product_df['optionName'].values.tolist()}
             order_dict['menus'].append(product_dict)
         order_list.append(order_dict)
 
