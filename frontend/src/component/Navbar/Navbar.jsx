7e79dc15b1f70b03125ef2cde016039900c652f3
jeongyeob97 <leejeongyeob97@gmail.com>
2020-12-04 16:27:14 +0900

    cba

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            {/* 여기는 navbar. 메뉴통계랑 옵션통계는 드롭다운으로 구현해야 함! */}
-            {/* 왜 이름바뀌는건 안대는거냐 */}
-            <Link to="/admin/">MAKE SALAD</Link>
-            <Link to="/admin/menustat">메뉴통계</Link>
-            <Link to="/admin/optionstat">옵션통계</Link>
-            <Link to="/admin/menuadmin">메뉴관리</Link>
+        // className={styles.className}
+        <nav className={styles.navbar}>
+            <div className = {styles.navCenter}>
+                <div className = {styles.navTitle}>
+                    <Link to= "/admin/" className = {styles.title}>Make Salad</Link>
+                </div>
+            </div>
+            <div className = {styles.navCenter}>
+                <ul className = {styles.navBtn}>
+                    <li>
+                    <Link to="/admin/menustat" className = {styles.navList}>메뉴 관리</Link>
+                    </li>
+                    <li>
+                    <Link to="/admin/optionstat" className = {styles.navList}>매출 통계</Link>
+                    </li>
+                    {/* <li>
+                    <Link to="/admin/menuadmin" className = {styles.navList}>메뉴관리</Link>
+                    </li> */}
+                </ul>
+            </div>
+            {/* <ul>
+                <Link to="/admin/">MAKE SALAD</Link>
+                <Link to="/admin/menustat">메뉴통계</Link>
+                <Link to="/admin/optionstat">옵션통계</Link>
+                <Link to="/admin/menuadmin">메뉴관리</Link>
+            </ul> */}
         </nav>
     );
 };
+
+
 
 export default Navbar;
