df7f8e779215399da579555a2688dc4139b59d5a
kjh03160 <kis03160@likelion.org>
2020-12-05 01:28:14 +0900

    Kitchen css

--- frontend/src/container/CompletedMenuContainer.js
+++ frontend/src/container/CompletedMenuContainer.js
@@ -4,13 +4,13 @@ import {useAsync} from 'react-async'
 
 import OrderAPI from '../api/orderAPI'
 
-import { BiCheckSquare } from "react-icons/bi";
+import { IoRestaurantOutline } from "react-icons/io5";
 import  styled from 'styled-components'
 
 
 const Wrapper = styled.div`
 display:flex;
-width: 55%;
+width: 85%;
 height:100%;
 border:1px solid black;
 
@@ -24,22 +24,44 @@ flex-direction:column;
 overflow-y:scroll;
 `
 const Orders = styled.div`
-span{
+span.orderId{
   display:flex;
   justify-content:center;
   align-items:center;
   width:100%;
+  color:white;
+  font-size:large
 }
 width:100%;
 height:50px;
 display:flex;
 border:1px solid black;
 cursor: pointer;
+
+background-color: skyblue;
+border: 2px solid black;
+border-radius: 1px;
 `
 const DetailWrapper = styled.section`
   width:70%;
   height:100%;
   overflow-y:scroll;
+  padding:5% 5%;
+  font-size:30px;
+  div.orderId{
+    font-size: 50px
+  }
+  div.count{
+    text-align:right;
+  }
+`
+
+const MenuWrapper = styled.section`
+  font-size: 30px
+`
+
+const OptionWrapper = styled.section`
+  text-indent: 3em;
 `
 
 function DetailSection(props) {
@@ -49,14 +71,24 @@ function DetailSection(props) {
     <DetailWrapper >
     {value === index && (
       <>
-        <div >주문번호 : {detail.orderPk}</div>
+        <div class="orderId">주문번호 : {detail.orderPk}</div>
         <div>
           {detail.menus.map((menu,index)=>
-            (<div key ={index}>
-              {menu.menuName}
-              {menu.options.map((options,index)=><div key ={index}>{options}</div>)}
-              <div key = {index}>{menu.quantity}개</div>
-            </div>)
+            (
+            <div key ={index}>
+              <MenuWrapper>
+              메뉴{index + 1} : {menu.menuName}<br/>
+              </MenuWrapper>
+              {
+                menu.options[0] !== null ?
+              <OptionWrapper>
+              {menu.options.map((options,index)=><div class="option" key ={index}>- {options} 추가</div>)}
+              </OptionWrapper>: null
+              }
+              <div class='count' key = {index}>수량 : {menu.quantity}개</div>
+              <br/>
+            </div>
+            )
           )}
         </div>
       </>
@@ -96,15 +128,17 @@ export default function CompletedMenuContainer() {
         {data.orderList.map((order,index)=>{
           if (order.completed === false) 
           return (
-                  <>
-                  <Orders key ={order.orderPk} onClick={(event)=>handleChange(event,order.orderPk)} value={order.orderPk} >
-                    <span key ={index}>
+            // <OrderWrapper>     
+            <>             
+              <Orders key ={order.orderPk} onClick={(event)=>handleChange(event,order.orderPk)} value={order.orderPk} >
+                    <span class="orderId" key ={index}>
                       {`${order.orderPk}번 주문`}
                     </span>
-                    <BiCheckSquare style={{color:"red",width:"35px",height:"35px"}} onClick={(event)=>handleDone(order.orderPk)}/>
+                    <IoRestaurantOutline style={{color:"#4646CD",width:"35px",height:"35px"}} onClick={(event)=>handleDone(order.orderPk)}/>
                   </Orders> 
                   
-                  </>
+                  {/* </OrderWrapper> */}
+            </>
                   )     
           })}
       </OrderSection>
--- frontend/src/container/SoldOutMenuContainer.js
+++ frontend/src/container/SoldOutMenuContainer.js
@@ -1,59 +1,180 @@
 import React from 'react'
-import {useAsync} from 'react-async'
-import  styled ,{css}from 'styled-components'
+import { useAsync } from 'react-async'
+import styled, { css } from 'styled-components'
 import MenuAPI from '../api/saveData'
 const Wrapper = styled.section`
   width:45%;
   height:100%;
   border:1px solid black;
-   
+  background-color:#dcdcdc;
+  h2{
+    font-size:40px;
+  }
+  h3{
+    font-size:30px;
+    text-indent: 1em;
+  }
+  padding-left:1%;
 `
 const Text = styled.div`
   color: #495057;
   cursor: pointer;
+  text-indent: 3em;
+  margin-top:1%;
+  margin-bottom:2%;
   ${props =>
     props.SoldOut &&
     css`
-      color: red;
-    `}
+    color: red;
+    text-decoration:line-through;
+  `}
+
+
+
+  .tasks-list-item {
+    display: block;
+    line-height: 24px;
+    padding: 12px 15px;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  
+  .tasks-list-item + .tasks-list-item {
+    border-top: 1px solid #f0f2f3;
+  }
+  
+  .tasks-list-cb {
+    display: none;
+  }
+  
+  .tasks-list-mark {
+    position: relative;
+    display: inline-block;
+    vertical-align: center;
+    margin-left: 12px;
+    width: 20px;
+    height: 20px;
+    border: 2px solid white;
+    // border-radius: 12px;
+  }
+  
+  .tasks-list-mark:before {
+    content: '';
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: -5px 0 0 -6px;
+    height: 4px;
+    width: 8px;
+    border: solid white;
+    border-width: 0 0 4px 4px;
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -ms-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+  }
+  
+  .tasks-list-cb:checked ~ .tasks-list-mark {
+    border-color: white;
+    background-color: red;
+  }
+  
+  .tasks-list-cb:checked ~ .tasks-list-mark:before {
+    display: block;
+  }
+  
+  .tasks-list-desc {
+    font-weight: bold;
+    color: #8a9a9b;
+  }
+  
+  .tasks-list-cb:checked ~ .tasks-list-desc {
+    color: #34bf6e;
+    text-decoration: line-through;
+  }
+
+
+
+
 `
-async function getMenu(){
+
+const Butt = styled.button`
+font-size:15px;
+:hover {
+  background: #464646	;
+  color:white;
+}
+    padding: 10px;
+    color: black;
+    border: 2px solid white;
+    text-decoration: none;
+    cursor: pointer;
+    transition: all 0.3s ease-out;
+    font-weight:bold;
+    ${props =>
+      props.SoldOut &&
+      css`
+      color: red;
+      text-decoration:line-through;
+    `}
+
+`
+async function getMenu() {
   const response = await MenuAPI.getAll()
   return response.data
 }
 
 export default function SoldOutMenuContainer() {
-  const {data, error, isLoading,reload} = useAsync({
-    promiseFn:getMenu
+  const { data, error, isLoading, reload } = useAsync({
+    promiseFn: getMenu
   })
 
-  async function handleMainClick(menuPk){
-     let response = await MenuAPI.setSoldout({pk:menuPk,type:"main_soldout"})
-     reload()
-     return response
-   }
-   async function handleOptionClick(optionPk){
-     let response = await MenuAPI.setSoldout({pk:optionPk,type:"option_soldout"})
-     reload()
-     return response
-   }
-  
-  if(isLoading) return <div>loading</div>
+  async function handleMainClick(menuPk) {
+    let response = await MenuAPI.setSoldout({ pk: menuPk, type: "main_soldout" })
+    reload()
+    return response
+  }
+  async function handleOptionClick(optionPk) {
+    let response = await MenuAPI.setSoldout({ pk: optionPk, type: "option_soldout" })
+    reload()
+    return response
+  }
+
+  if (isLoading) return <div>loading</div>
   return (
     <Wrapper>
-        <h2>메뉴 품절 관리 </h2>
-        {data.category.map((category)=> 
-          <div>
-            <h2>{category.categoryName}</h2>
-            
-              {data.main.filter((main)=>main.categoryPk == category.categoryPk).map(
-              (main,index)=><Text key={index} SoldOut = {main.menuSoldout} onClick={()=>handleMainClick(main.menuPk)}> {main.menuName}</Text>
-            )}
-            
-          </div>
-        )}
-        <h2>옵션 품절 관리</h2>
-        {data.option.map((option,index)=><Text key={index} onClick ={()=>handleOptionClick(option.optionPk)}SoldOut = {option.optionSoldout} > {option.optionName}</Text>)}
+      <h2>메뉴 품절 관리 </h2>
+      {data.category.map((category) =>
+        <div>
+          <h3>{category.categoryName}</h3>
+
+          {data.main.filter((main) => main.categoryPk == category.categoryPk).map(
+            (main, index) =>           
+            <Text class="box"  onClick={() => handleMainClick(main.menuPk)} ><Butt class="button" key={index} SoldOut={main.menuSoldout} > {main.menuName}</Butt>
+            {main.menuSoldout === 0 ?     
+              <input type="checkbox" name="task_1" value="1" class="tasks-list-cb" ></input>
+            : <input type="checkbox" name="task_1" value="1" class="tasks-list-cb" checked></input>}
+            <span class="tasks-list-mark"></span>  </Text>
+          
+          
+          )}
+
+        </div>
+      )}
+      <h2>옵션 품절 관리</h2>
+      {data.option.map((option, index) => <Text class="box"  onClick={() => handleOptionClick(option.optionPk)} ><Butt class="button" key={index} SoldOut={option.optionSoldout} > {option.optionName}</Butt>
+      {option.optionSoldout === 0 ?     
+        <input type="checkbox" name="task_1" value="1" class="tasks-list-cb" ></input>
+      : <input type="checkbox" name="task_1" value="1" class="tasks-list-cb" checked></input>}
+      <span class="tasks-list-mark"></span>  </Text>)}
+
+
     </Wrapper>
+
   )
 }
