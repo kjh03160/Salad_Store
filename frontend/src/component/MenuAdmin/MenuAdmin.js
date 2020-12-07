604be603f3c56d244dd99f7685984eaf46947eec
JungTag <dbsdudxor121@naver.com>
2020-12-07 09:52:57 +0900

    메뉴관리 주석 추가 및 에러 수정

--- frontend/src/component/MenuAdmin/MenuAdmin.js
+++ frontend/src/component/MenuAdmin/MenuAdmin.js
@@ -4,21 +4,20 @@ import styles from './MenuAdmin.module.css';
 import { Multiselect } from 'multiselect-react-dropdown';
 
 const MenuAdmin = (props) => {
-    // 데이터에 관련된 상태를 선언
+    // 데이터에 관련된 상태
     const [relations, setRelations] = useState([]);
     const [mains, setMains] = useState([]);
     const [options, setOptions] = useState([]);
     const [categories, setCategories] = useState([]);
-    // 추가를 위한 상태를 선언
+    // 입력시 이벤트를 위한 상태
     const [selectedOptions, setSelectedOptions] = useState([]);
     const [isCatOn, setIsCatOn] = useState(false);
     const [isMenuOn, setIsMenuOn] = useState([]);
     const [isOptOn, setIsOptOn] = useState(false);
     const [isSelectorOn, setIsSelectorOn] = useState([]);
-
+    // 입력값을 참조하는 데 사용
     const catInputRef = React.createRef();
     const catFormRef = React.createRef();
-
     const optionInputRef = React.createRef();
     const optionPriceRef = React.createRef();
     const optionFormRef = React.createRef();
@@ -26,7 +25,6 @@ const MenuAdmin = (props) => {
     // 데이터를 동기화시킨다.
     const apiCall = async () => {
         const response = await menuApi.getAll();
-        console.log(response.data);
         const { relation, main, option, category } = response.data;
         let menuOnArray = [];
         let selectorOnArray = [];
@@ -66,15 +64,10 @@ const MenuAdmin = (props) => {
         setRelations(relation);
     }
 
+    // 페이지 최초 로딩시 apiCall함수를 호출
     useEffect(() => {
         apiCall();
     }, []);
-
-    // useEffect(() => {
-    //     apiCall();
-    //     // axios를 이용해 데이터를 가져온다
-    // }, [btnClicked]);
-
 
     const getMatchedMains = (category, mains) => {
         return mains.filter(main => category.categoryPk === main.categoryPk);
@@ -94,24 +87,16 @@ const MenuAdmin = (props) => {
         }
         return result;
     };
-
-    const handleMatchedOptionDelete = async (e, menuPk, optionPk) => {
-        let response = await menuApi.deleteLink({menu_pk:menuPk, option_pk:optionPk});
-        optionCall();
-    };
-
+    
+    // 카테고리 추가&제거
     const handleCategoryAdd = async (name) => {
         const data = { category_name: name };
         let response = await menuApi.addCategory(data);
         categoryCall();
-
-        // setCategories(newCategory);
     };
 
     const handleCategoryDelete = async (e, categoryPk) => {
-        // let newCategories = [...categories];
         let data = { 'category_pk': categoryPk };
-
         let response = await menuApi.deleteCategory(data);
         categoryCall();
     };
@@ -122,7 +107,9 @@ const MenuAdmin = (props) => {
         handleCategoryAdd(name);
         catFormRef.current.reset();
     };
+    //
 
+    // 메뉴 추가&제거
     const handleMenuAdd = async (name, price, categoryPk, image) => {
         let data = new FormData();
         data.append('category_pk', categoryPk);
@@ -142,9 +129,11 @@ const MenuAdmin = (props) => {
 
     const onMenuSubmit = (e, categoryPk) => {
         e.preventDefault();
+        console.log(e);
         const name = e.target[0].value;
         const price = parseInt(e.target[1].value);
-        const image = e.target[2].files[0];
+        const image = e.target[3].files[0];
+        // 가격이 정수형이 아닐 때 에러 헨들링
         if (isNaN(price)) {
             e.target.reset();
         }
@@ -153,7 +142,9 @@ const MenuAdmin = (props) => {
             e.target.reset();
         }
     };
+    //
 
+    // 옵션 추가&제거
     const handleOptionAdd = async (name, price) => {
         const data = { 'option_name': name, 'option_price': price, 'option_soldout': 0 };
         let response = await menuApi.addOption(data);
@@ -170,7 +161,8 @@ const MenuAdmin = (props) => {
         e.preventDefault();
         const name = optionInputRef.current.value;
         const price = parseInt(optionPriceRef.current.value);
-        if (isNaN(price)) {
+        // 가격이 정수형이 아닐 때 에러 헨들링
+        if (isNaN(price)) { 
             optionFormRef.current.reset();
         }
         else {
@@ -178,9 +170,9 @@ const MenuAdmin = (props) => {
             optionFormRef.current.reset();
         }
     };
+    //
 
-    // 옵션 선택 event 처리
-
+    // 옵션-메뉴 연결 및 제거
     const onSubmit = async (e, menuPk) => {
         const newRelation = [...relations];
         selectedOptions.map(function (option) {
@@ -199,8 +191,13 @@ const MenuAdmin = (props) => {
         setSelectedOptions([...selectedList]);
     };
 
+    const handleMatchedOptionDelete = async (e, menuPk, optionPk) => {
+        let response = await menuApi.deleteLink({menu_pk:menuPk, option_pk:optionPk});
+        optionCall();
+    };
     //
 
+    // 입력 시 이벤트 처리
     const handleCatToggle = () => {
         const isOn = isCatOn;
         setIsCatOn(!isOn);
@@ -248,6 +245,7 @@ const MenuAdmin = (props) => {
             }
         }
     }
+    //
 
 
 
@@ -256,7 +254,8 @@ const MenuAdmin = (props) => {
             <div className={styles.title}>
                 <h1>메뉴 관리</h1>
             </div>
-            <div className={styles.menuAdminContent}>
+            {/* 카테고리&메뉴 관리 + 옵션 선택 */}
+            <div className={styles.menuAdminContent}> 
                 <div className={styles.content}>
                     <button onClick={handleCatToggle} className={styles.btn}>
                         <i className="fas fa-plus-square"></i>
@@ -271,6 +270,7 @@ const MenuAdmin = (props) => {
                         </form>
                     </div>
                     <br />
+                    {/* 카테고리 */}
                     {categories.map((category) => (
                         <div className={styles.category} key={category.categoryPk}>
                             <p className={styles.categoryName}>[{category.categoryName}]</p>
@@ -290,14 +290,14 @@ const MenuAdmin = (props) => {
                                         <i class="fas fa-check-square"></i>
                                     </button>
                                     <label className={styles.uploadIcon} htmlFor="inputFile">
+                                        <input type="file" id="inputFile" name="menuImage" className={styles.inputFile} />
                                         <i class="fas fa-cloud-upload-alt"></i>
                                     </label>
-                                    <input type="file" id="inputFile" name="menuImage" className={styles.inputFile} />
                                 </form>
                             </div>
+                            {/* 각 카테고리에 해당하는 메인 메뉴 */}
                             {getMatchedMains(category, mains).map((main) => (
                                 <div className={styles.main} key={main.menuPk}>
-                                    {/* <img className={styles.mainImage} src={main.menuImage} /> */}
                                     <p className={styles.mainName}>{main.menuName}</p>
                                     <p className={styles.mainPrice}>: {main.menuPrice}원</p>
                                     <button className={styles.delBtn} onClick={(e) => { if (window.confirm('해당 메뉴를 삭제하시겠습니까?')) handleMenuDelete(e, main.menuPk) }}>
@@ -311,11 +311,11 @@ const MenuAdmin = (props) => {
                                         <p className={getSelectorOn(main.menuPk) ? styles['hidden'] : styles['selectorToggle']}>옵션 선택</p>
                                         <form id={main.menuPk} className={getSelectorOn(main.menuPk) ? styles['optionSelector'] : styles['hidden']} onSubmit={(e) => onSubmit(e, main.menuPk)}>
                                             <Multiselect
-                                                options={options} // Options to display in the dropdown
-                                                onSelect={onSelect} // Function will trigger on select event
+                                                options={options} 
+                                                onSelect={onSelect} 
                                                 placeholder="옵션을 선택해주세요"
-                                                onRemove={onRemove} // Function will trigger on remove event
-                                                displayValue="optionName" // Property name to display in the dropdown options
+                                                onRemove={onRemove}
+                                                displayValue="optionName"
                                                 showCheckbox={true}
                                             />
                                             <button className={styles.addBtn}>
@@ -323,6 +323,7 @@ const MenuAdmin = (props) => {
                                             </button>
                                         </form>
                                     </div>
+                                    {/* 각 메인 메뉴에 해당하는 옵션 */}
                                     <div className={styles.matchedOptions}>
                                         {getMatchedOptions(main, options).map((option) => (
                                             <div className={styles.option} key={option.optionPk}>
@@ -340,6 +341,7 @@ const MenuAdmin = (props) => {
                         </div>
                     ))}
                 </div>
+                {/* 옵션 관리 */}
                 <div className={styles.modifyOption}>
                     <div className={styles.optionContent}>
                         <button onClick={handleOptToggle} className={styles.btn}>
@@ -357,7 +359,6 @@ const MenuAdmin = (props) => {
                         </div>
                         <br />
                         {options.map((option) => (
-                            // css할 때는 className 변경해야 함!
                             <div className={styles.rightOption} key={option.optionPk}>
                                 <p className={styles.rightOptionName}>{option.optionName}</p>
                                 <p className={styles.rightOptionPrice}> : {option.optionPrice}원</p>
