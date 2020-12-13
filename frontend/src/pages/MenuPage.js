e206fa66f20353f42d3e1e9a68573c80fcf48901
JungTag <dbsdudxor121@naver.com>
2020-12-13 21:25:17 +0900

    delete trash files

--- frontend/src/pages/MenuPage.js
+++ frontend/src/pages/MenuPage.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import MenuContainer from '../container/MenuContainer'
-
-
 import '../App.css';
 
-
-
 export default function MenuPage(props){
-  // 코드 넘 더럽...ㅠ
     return(
       <>
           <MenuContainer />
